8ec7c45af1bf42cb00d6aa21021d578e69477d7f
Chertiila <daniilp737@gmail.com>
2021-12-15 19:03:07 +0300

    1

--- Assets/II.cs
+++ Assets/II.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class II : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float random_point_radius=100;
+    Vector3 random_point;
+    public Transform player;
+    public GameObject random_point_prefab;
+    Transform near_player;
+    Transform my_transform;
+    Transform target;
+    NavMeshAgent agent;
+    NavMeshPath nav_mesh_path;
+
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        my_transform = GetComponent<Transform>;
+        target = random_point_radius;
+        near_player = Instantiate()
     }
 
     // Update is called once per frame
